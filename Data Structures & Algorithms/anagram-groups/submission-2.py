1783216a87475d040759508db5fa361bca3a6433
Samuel Lucca Viana Miranda <85247638+Samuel-Wisart@users.noreply.github.com>
2026-08-03 17:21:57 -0300

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = []
+        # Count chars
+        for w in strs:
+            wordmap = {}
+            for c in w:
+                wordmap[c] = wordmap.get(c, 0) + 1
+
+            for i, group in enumerate(res):
+                firstwordmap = {}
+                for c in group[0]:
+                    firstwordmap[c] = firstwordmap.get(c, 0) + 1 
+                if firstwordmap == wordmap:
+                    res[i].append(w)
+                    break
+            else:
+                group = [w]
+                res.append(group)
+                
+        return res
+
+
+        
+        
+        
+
+            
